0658e980406823a4d9d252d419e35bfca78b38a6
ashleykolodziej <kolodz10@bu.edu>
2018-09-14 14:08:21 -0400

    Add a build task for consistency

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,5 +49,6 @@ module.exports = function(grunt) {
 
 	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
 	grunt.registerTask( 'styles',   [ 'sass' ] );
+	grunt.registerTask( 'build',    [ 'styles' ] );
 	grunt.registerTask( 'default',  [ 'watch' ] );
 };
