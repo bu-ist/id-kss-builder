82053206cded1ff4c488adac5b6385bb4669765c
ashleykolodziej <kolodz10@bu.edu>
2018-09-17 14:22:03 -0400

    Allow themes to set breakpoints in future

--- kss-assets/kss-breakpoints.js
+++ kss-assets/kss-breakpoints.js
@@ -4,15 +4,41 @@
   var KssBreakpoints = function (config) {
     this.bodyClass = config.bodyClass || 'kss-breakpoints-mode';
 
+    var defaultBreakpoints = {
+      min: '320',
+      xs: '500',
+      sm: '768',
+      md: '992',
+      lg: '1200',
+    }
+
+    this.allowedBreakpoints = config.allowedBreakpoints || defaultBreakpoints;
+
     this.init();
   };
 
   KssBreakpoints.prototype.init = function () {
-    var self = this;
+    var self = this,
+        breakpointsControlContainer = document.getElementById('id-kss-breakpoints-control');
+
+    self.createBreakpointsControl(self, breakpointsControlContainer);
+
     // Initialize all guides toggle buttons.
     var elementList = document.getElementsByClassName('id-kss-breakpoints-control-button');
+    elementList[0].classList.add('id-kss-breakpoints-control-active');
+    elementList[0].childNodes.checked = true;
+
     for (var button of elementList) {
       button.onclick = self.updateBreakpoints.bind(self);
+    }
+  };
+
+  // Create breakpoint controls
+  KssBreakpoints.prototype.createBreakpointsControl = function (self, container) {
+    container.innerHTML += '<span class="id-kss-toolbar-label">View examples as:</span>';
+
+    for (var breakpoint in self.allowedBreakpoints) {
+      container.innerHTML += '<label class="id-kss-breakpoints-control-button id-kss-breakpoints-control-' + breakpoint + '"><input type="radio" name="breakpoint" value="' + self.allowedBreakpoints[breakpoint] + '"> ' + breakpoint + '</label>';
     }
   };
 
@@ -25,8 +51,6 @@
     for (var button of active) {
       button.classList.remove('id-kss-breakpoints-control-active')
     }
-
-    console.log(self);
 
     self.explicitOriginalTarget.classList.add('id-kss-breakpoints-control-active');
   };
