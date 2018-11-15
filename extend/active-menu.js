fe8068c54f27528abea026e83c1f1a57cb58852e
acketon <acketon@gmail.com>
2018-11-15 12:36:53 -0500

    support sidebar nav on items
    modify so that if isActive is found on a child item figure out the parent menu based on the sectionReference that it is in. So an item under Blocks->Global->My Cool Block will match the main Blocks menu so that all menu items under “Blocks” will be displayed in the sidebar nav for the item page “My Cool Block”

--- extend/active-menu.js
+++ extend/active-menu.js
@@ -7,14 +7,31 @@ module.exports = (handlebars) => {
 				const menuItem = menuItems[i];
 
 				if (menuItem.isActive) {
+					//console.log(menuItem);
 					return menuItem;
 				}
 
+
+				//gets child menu items nested menu
 				const activeMenu = getActiveMenu(menuItem.children);
 
 				if (activeMenu) {
+
+
+					var section = activeMenu.sectionreferenceURI;
+
+
+					for (let i = 0; i < nestedMenu.length; i += 1) {
+						if (nestedMenu[i].referenceURI == section) {
+
+							return nestedMenu[i];
+						}
+					}
+
 					return activeMenu;
 				}
+
+
 			}
 
 			return null;
