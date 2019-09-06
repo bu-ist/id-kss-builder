0382517b59808ec21f61a2ebc403f5ef475f2aac
ashleykolodziej <kolodz10@bu.edu>
2019-09-06 16:02:14 -0400

    Try ajax load

--- kss-assets/kss-sidebar.js
+++ kss-assets/kss-sidebar.js
@@ -1,49 +1,13 @@
-(function (window, document) {
-  'use strict';
-
-  // Set the configuration values on object creation.
-
-  var KssSidebar = function (config) {
-    this.bodyClass = config.bodyClass || 'kss-sidebar-open';
-    this.init();
-  };
-
-  // Initialize the page to see if the sidebar should be immediately
-  // turned on.
-  KssSidebar.prototype.init = function () {
-
-
-    var self = this;
-    // Initialize sidebar button.
-    var checkbox = document.querySelectorAll('.id-kss-sidebar-control input')[0];
-    if(!checkbox) {
-      //toolbar/sidebar not present
-      return false;
-    }
-    //set the sidebar class
-    self.toggleSidebar( checkbox.checked );
-
-    // setup an event listener on the checkbox
-    checkbox.addEventListener('change', function(event) {
-      console.log(event.target.checked);
-      self.toggleSidebar( event.target.checked );
-    });
-
-  };
-
-  KssSidebar.prototype.toggleSidebar = function( status ) {
-
-    if ( status == false ) {
-      //not checked so hide sidebar
-      document.body.classList.remove( this.bodyClass );
-
-    } else {
-      //assume checked and sidebar visible
-      document.body.classList.add( this.bodyClass );
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("id-kss-sidebar").innerHTML =
+    this.responseText;
     }
   };
+  xhttp.open("GET", "index.html#id-homepage-nav", true);
+  xhttp.send();
+}
 
-  // Export to DOM global space.
-  window.KssSidebar = KssSidebar;
-
-})(window, document);
+loadDoc();
